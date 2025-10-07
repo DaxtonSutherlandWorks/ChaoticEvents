69fbe36d03fb10b61ee019f7bc145710d44daab8
DaxtonSutherlandWorks <daxtonas77@gmail.com>
2025-10-07 02:24:10 -0400

    Cors update

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use((req, res, next) => {
+    res.setHeader(['Access-Control-Allow-Origin','https://chaoticevents-frontend.onrender.com/']);
     next();
 });
 
